2bf09522ff1db6dfa27f13dab98c6ea78f761465
mdeniz <mdeniz@suse.com>
2017-02-21 11:21:19 +0100

    [dist] Fix minor bug in provisioner script

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 zypper -q -n install --replacefiles plymouth plymouth-scripts plymouth-plugin-script plymouth-x11-renderer plymouth-plugin-label sed
 zypper -q -n up
-cd /vagrant && ./export.sh  "source/sandbox" "preview_theme" && ./install.sh "preview_theme"
+cd /vagrant && ./export.sh  "source/sandbox/default" "preview_theme" && ln -sf "/vagrant/export/preview_theme" /usr/share/plymouth/themes/ && plymouth-set-default-theme -R "preview_theme"
