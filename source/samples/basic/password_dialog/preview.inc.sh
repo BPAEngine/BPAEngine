4060933718c090b25417177328105bedd1acc14c
mdeniz <mdeniz@suse.com>
2017-02-24 17:02:00 +0100

    [engine] Add sample for PasswordDialog

---
+++ source/samples/basic/password_dialog/preview.inc.sh
@@ -0,0 +1,7 @@
+plymouth ask-for-password --prompt="Introduce the password"
+
+plymouth change-mode --shutdown
+
+plymouth ask-for-password --prompt="Introduce the password"
+
+plymouth unpause-progress
