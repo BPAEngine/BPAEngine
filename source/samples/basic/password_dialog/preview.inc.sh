80e83fe104587a2e109e6319fe2bd5a92baa146b
mdeniz <mdeniz@suse.com>
2017-02-24 17:43:00 +0100

    [engine] wait a bit between asking for passwords

--- source/samples/basic/password_dialog/preview.inc.sh
+++ source/samples/basic/password_dialog/preview.inc.sh
@@ -2,6 +2,8 @@ plymouth ask-for-password --prompt="Introduce the password"
 
 plymouth change-mode --shutdown
 
+sleep 1
+
 plymouth ask-for-password --prompt="Introduce the password"
 
 plymouth unpause-progress
