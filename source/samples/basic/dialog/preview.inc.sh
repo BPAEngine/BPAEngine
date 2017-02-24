eaa24a904b1d7382af83c62fe93cf378965b379b
mdeniz <mdeniz@suse.com>
2017-02-24 13:38:52 +0100

    [samples] Improve sample for Dialogs, showing 2 kinds of configs in one preview

--- source/samples/basic/dialog/preview.inc.sh
+++ source/samples/basic/dialog/preview.inc.sh
@@ -1,10 +1,18 @@
 QUESTIONS=("What is your name?"
-           "How old are you?"
-           "How many kids do you have?")
+           "Do you like this feature?")
 IFS=""
 for question in ${QUESTIONS[*]}; do
   sleep 1
   echo "Asking a question: ${question}"
   plymouth ask-question --prompt="${question}"
 done
+
+plymouth change-mode --shutdown
+
+for question in ${QUESTIONS[*]}; do
+  sleep 1
+  echo "Asking a question: ${question}"
+  plymouth ask-question --prompt="${question}"
+done
+
 plymouth unpause-progress
