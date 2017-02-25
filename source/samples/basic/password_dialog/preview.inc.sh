e46fa131373a4a3351b3971925097c2c9a91e442
mdeniz <mdeniz@suse.com>
2017-02-25 19:02:11 +0100

    [samples] More realistic password prompt for PasswordDialog sample

--- source/samples/basic/password_dialog/preview.inc.sh
+++ source/samples/basic/password_dialog/preview.inc.sh
@@ -1,9 +1,9 @@
-plymouth ask-for-password --prompt="Introduce the password"
+plymouth ask-for-password --prompt="Please enter passphrase for device ASHSDIFHASLFKAJSFKLSJF (H%&$%&/fSDFSDLFSKÑSLK43059u3kl5j3lñ)"
 
 plymouth change-mode --shutdown
 
 sleep 1
 
-plymouth ask-for-password --prompt="Introduce the password"
+plymouth ask-for-password --prompt="Please enter passphrase for device ASHSDIFHASLFKAJSFKLSJF (H%&$%&/fSDFSDLFSKÑSLK43059u3kl5j3lñ)"
 
 plymouth unpause-progress
