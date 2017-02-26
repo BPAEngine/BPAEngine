472f5cd79276bd78a848b51efad2d54e9b8ea98a
mdeniz <mdeniz@suse.com>
2017-02-26 03:03:42 +0100

    [samples] Remove unneeded sleep

--- source/samples/basic/password_dialog/preview.inc.sh
+++ source/samples/basic/password_dialog/preview.inc.sh
@@ -2,8 +2,6 @@ plymouth ask-for-password --prompt="Please enter passphrase for device ASHSDIFHA
 
 plymouth change-mode --shutdown
 
-sleep 1
-
 plymouth ask-for-password --prompt="Please enter passphrase for device ASHSDIFHASLFKAJSFKLSJF (H%&$%&/fSDFSDLFSKÑSLK43059u3kl5j3lñ)"
 
 plymouth unpause-progress
