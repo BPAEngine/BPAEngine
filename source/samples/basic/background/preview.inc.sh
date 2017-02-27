cb14e2adf1a13b62230e7e37ee48bbf473b69473
mdeniz <mdeniz@suse.com>
2017-02-27 12:17:48 +0100

    [samples] Preview Background sample in 3 steps

--- source/samples/basic/background/preview.inc.sh
+++ source/samples/basic/background/preview.inc.sh
@@ -1,1 +1,9 @@
-sleep $DURATION
+STEP_DURATION=$(($DURATION / 3))
+
+sleep $STEP_DURATION
+
+plymouth change-mode --shutdown
+sleep $STEP_DURATION
+
+plymouth change-mode --updates
+sleep $STEP_DURATION
