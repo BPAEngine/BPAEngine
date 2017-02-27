0c63b54e470034974a26d4c7b34604cf821dce1b
mdeniz <mdeniz@suse.com>
2017-02-27 13:59:20 +0100

    [samples] Wait 1 sec after hiding each password dialog

--- source/samples/basic/password_dialog/preview.inc.sh
+++ source/samples/basic/password_dialog/preview.inc.sh
@@ -1,7 +1,11 @@
 plymouth ask-for-password --prompt="Please enter passphrase for device ASHSDIFHASLFKAJSFKLSJF (H%&$%&/fSDFSDLFSKÑSLK43059u3kl5j3lñ)"
 
+sleep 1
+
 plymouth change-mode --shutdown
 
 plymouth ask-for-password --prompt="Please enter passphrase for device ASHSDIFHASLFKAJSFKLSJF (H%&$%&/fSDFSDLFSKÑSLK43059u3kl5j3lñ)"
+
+sleep 1
 
 plymouth unpause-progress
